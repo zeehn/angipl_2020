fd4baeb11d1d84c7b884df6a1250020a6e623648
Zakir Hussain <zack.mesacc@gmail.com>
2020-04-30 16:18:58 +0500

    Change db from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_30_100621) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "words", force: :cascade do |t|
     t.string "value"
     t.string "language"
