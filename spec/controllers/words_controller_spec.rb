10da695be2945e9cff921452919cd4328bfd2f15
Zakir Hussain <zack.mesacc@gmail.com>
2020-05-02 13:47:25 +0500

    Add specs for create and new actions"

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do 
+  # index method specs
   describe "GET index" do
     before { get :index }
 
@@ -26,4 +27,42 @@ RSpec.describe WordsController, type: :controller do
       end
     end
   end
+
+  # new method specs
+  describe "GET new" do
+    before { get :new }
+
+      let!(:word) { create(:word) }
+
+      it "assigns @word" do
+        expect(assigns(:word)).to be_a_new(Word)
+      end
+
+      it "renders the new template" do
+        expect(response).to render_template(:new)
+      end
+  end
+
+  describe "POST create" do
+    subject { post :create, params: params }
+
+    context 'when valid params' do
+      let(:params) do 
+        { word: { value: 'dog', language: 'english' } }
+      end
+      it 'creates a new word' do  
+        expect { subject }.to change(Word, :count).from(0).to(1)
+      end
+    end
+
+    context 'when invalid params' do
+      let(:params) do 
+        { word: { value: 'dog' } }
+      end
+      it 'does not creates a new word' do  
+        expect { subject }.not_to change(Word, :count)
+      end
+    end
+  end
+
 end
